f8a10dad2d9fdcaa3c1b65fac030fc554498e90d
Steven Heinrich <steven.heinrich@gmail.com>
2015-03-08 09:41:13 -0400

    add basic map

---
+++ js/map.js
@@ -0,0 +1,14 @@
+// map object
+var map = L.map('map',{
+	scrollWheelZoom: false,
+	zoomControl: false
+}).setView([39.605046, -97.935209], 4);
+
+// basemap
+L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	id: 'hoganmaps.ikkpodh4'
+}).addTo(map);
