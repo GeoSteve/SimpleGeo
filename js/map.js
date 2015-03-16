4d9fe9c089c823d91e82de16bf770c35371b3b5a
Steven Heinrich <steven.heinrich@gmail.com>
2015-03-15 22:08:19 -0400

    add state data with simplified geojson layers

--- js/map.js
+++ js/map.js
@@ -12,3 +12,29 @@ L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
 		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
 	id: 'hoganmaps.ikkpodh4'
 }).addTo(map);
+
+$.getJSON('/data/us_states_simplified_high.geojson',function(data) {
+    L.geoJson(data, {
+        style: {
+            'fillColor': '#505050',
+            'weight': 1,
+            'opacity': 1,
+            'color': 'white',
+            'fillOpacity': 0.6
+        },
+        onEachFeature: function(feature,layer) {
+            layer.bindPopup(buildPopUpContent(feature.properties));
+        }
+    }).addTo(map);
+});
+
+function buildPopUpContent(properties) {
+    var content = '';
+    $.each(properties, function(index, value) {
+        content += index + ': ' + value + '<br/>';
+    });
+    return content;
+}
+
+
+// var censusAPI = 'http://api.census.gov/data/2010/sf1?key=730e49d850365e415931ee4f3a309ffea517b92d&get=P0010001&for=state';
